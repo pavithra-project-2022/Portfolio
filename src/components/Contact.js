b122bf90d3c49ebc0087f3c693256d9e39acd881
Pavithra Sethuraman <pavithrasethuraman92@gmail.com>
2022-05-14 05:29:59 +0530

    commited

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -121,6 +121,8 @@ const Contact = () => {
         <PowerButton />
         <ParticleComponent theme="light" />
         <Main>
+          <p>pavithrasethuraman92@gmail.com</p>
+          <p>8838647902</p>
           <Title>Contact Me</Title>
           <Description>
             <form  onSubmit={sendEmail}>
